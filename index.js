64259dc217b899262f7b3379d21e9f23a7998c50
Chas Belov <59780179+ChasBelov@users.noreply.github.com>
2025-11-02 18:45:59 +0000

    Update index.js
    Changed to copyable bookmarklet. Coding changes from the original source as described in README.md.

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const likeContainer = document.getElementById("like-button-renderer");
-const nextButton = document.getElementsByClassName("next-button")[0];
-
-if (!likeContainer || !nextButton) {
-  throw new Error("Failed to find dislike and next buttons");
-}
-
-setInterval(() => {
-  if (likeContainer.getAttribute("like-status") === "DISLIKE") {
-    nextButton.click();
-  }
-}, 1000);
+javascript:(function(){{if (window.skipset=="TRUE"){return;}};window.skipset="TRUE";const likeContainer = document.getElementById("like-button-renderer");const nextButton = document.getElementsByClassName("next-button")[0];if (!likeContainer || !nextButton) {   throw new Error("Failed to find dislike and next buttons");};setInterval(() => {if(likeContainer.getAttribute("like-status") === "DISLIKE") {nextButton.click();}}, 2000);})();
